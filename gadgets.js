927c3607ed9ba9946c4cd6e1758e6ee8b2192e88
snowfluff <70106649+gdsnowfluff@users.noreply.github.com>
2023-10-31 16:32:04 -0400

    formatting, removed notebook

---
+++ gadgets.js
@@ -0,0 +1,14 @@
+function cola()
+{
+    var colar = document.getElementById('colar');
+    var coluh = document.getElementById("coluh");
+    coluh.style.backgroundColor = colar.value;
+}
+
+function text()
+{
+    var tehxt = document.getElementById('tehxt');
+    var tex = document.getElementById('tex');
+
+    tex.innerHTML = tehxt.value;
+}
